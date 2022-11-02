2b4d9dae10ce196cb0e117739b29a5a3b9b8827f
MariaAsif135 <100357211+MariaAsif135@users.noreply.github.com>
2022-11-03 02:05:36 +0300

    signup page connected
    When the signup is clicked, it takes the user to sign in page

--- ProjectCode/playground/urls.py
+++ ProjectCode/playground/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 #URLConf
 urlpatterns = [
-path('hello/',views.say_hello),
+#path('hello/',views.say_hello),
 path('',views.home, name = "home"),
-path('hello/signup/',views.signup, name = "signup"),
-path('hello/signin/',views.signin, name = "signin"),
-path('hello/signout/',views.signout, name = "signout"),
+path('signup/',views.signup, name = "signup"),
+path('signin/',views.signin, name = "signin"),
+#path('signout/',views.signout, name = "signout"),
 
 ]
--- ProjectCode/playground/views.py
+++ ProjectCode/playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect
 def say_hello(request):
     x=1
     y=2
@@ -8,12 +10,28 @@ def say_hello(request):
     return render(request, 'index.html', {'name':'Maria'})
 
 def home(request):
-    return HttpResponse("Hello I am working")
+    return render(request, 'index.html', {'name':'HomePage'})
 
 def signout(request):
     pass
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        theuser = User.object.create_user(username,email, pass1)
+        theuser.first_name= fname
+        theuser.last_name= lname
+
+        theuser.save()
+        messages.success(request, "Account created Successfully")
+        return redirect('signin')
+
     return render(request, 'signup.html')
 
 def signin(request):
