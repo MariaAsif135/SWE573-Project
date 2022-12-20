af60eda8bbd32aa4aefe09039ea823247a9aaade
MariaAsif135 <100357211+MariaAsif135@users.noreply.github.com>
2022-12-20 11:47:57 +0300

    Unit tests added
    Tests created for possible sign in errors that may occur

--- ProjectCode/core/tests.py
+++ ProjectCode/core/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
+
+class Unit_Tests_For_Application(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="unittest", email="testing123@testing.com",password="password")
+        self.user.save()
+
+    def Signing_In_Unittest(self):
+        user = authenticate(username='unittest', password='password')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def Signing_In_With_Wrong_Username_Unittest(self):
+        user = authenticate(username='unittestwow', password='password')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def Signing_In_With_Wrong_Password_Unittest(self):
+        user = authenticate(username='unittest', password='passwordqwerty')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    
+
 
 # Create your tests here.
